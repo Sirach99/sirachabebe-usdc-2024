a5c7b391748a820c7c53f882c88939be338171a4
sirach99 <sirachabebe@Sirachs-MBP.fios-router.home>
2023-12-02 15:23:07 -0500

    more docblocks updates

--- book_search.js
+++ book_search.js
@@ -26,7 +26,7 @@
 }
 
 /**
- * 
+ * Adds scanned text to search results.
  * @param {Array} searchResults - Search results.
  * @param {Array} scannedTextWithSearchTerm - Scanned text which contains the search term.
  * @param {String} ISBN - Book identifier.
@@ -46,7 +46,7 @@ function addScannedTextToSearchResults(searchResults, scannedTextWithSearchTerm,
 }
 
 /**
- * 
+ * Gets all scanned text which contains the search term.
  * @param {Array} content - Scanned text for a particular book.
  * @param {String} searchTerm - The word or term we're searching for. 
  * @returns {Array} Scanned text which contains the search term.
